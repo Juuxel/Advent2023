5e3be20985caf3fb88631f8136e47cd285c00c58
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-12-06 22:55:59 +0200

    Day 6

---
+++ src/main/java/juuxel/advent2023/Day6.java
@@ -0,0 +1,61 @@
+package juuxel.advent2023;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Day6 {
+    public static void main(String[] args) throws Exception {
+        part1(Loader.lines(6).toArray(String[]::new));
+        part2(Loader.lines(6).toArray(String[]::new));
+    }
+
+    public static void part1(String[] lines) {
+        List<Race> races = readRaces(lines[0], lines[1]);
+        long result = races.stream()
+            .mapToLong(Race::waysToBeat)
+            .reduce(1, (a, b) -> a * b);
+        System.out.println(result);
+    }
+
+    public static void part2(String[] lines) {
+        Race race = readSingleRace(lines[0], lines[1]);
+        System.out.println(race.waysToBeat());
+    }
+
+    private static List<Race> readRaces(String times, String distances) {
+        String[] timeParts = times.split(" +");
+        String[] distanceParts = distances.split(" +");
+
+        List<Race> races = new ArrayList<>(timeParts.length - 1);
+
+        for (int i = 1; i < timeParts.length; i++) {
+            races.add(new Race(Long.parseLong(timeParts[i]), Long.parseLong(distanceParts[i])));
+        }
+
+        return races;
+    }
+
+    private static Race readSingleRace(String times, String distances) {
+        String[] timeParts = times.split(" +");
+        String[] distanceParts = distances.split(" +");
+
+        String singleTime = Arrays.stream(timeParts).skip(1).collect(Collectors.joining());
+        String singleDistance = Arrays.stream(distanceParts).skip(1).collect(Collectors.joining());
+        return new Race(Long.parseLong(singleTime), Long.parseLong(singleDistance));
+    }
+
+    private record Race(long time, long bestDistance) {
+        private long waysToBeat() {
+            double discriminator = Math.sqrt(time * time - 4 * bestDistance);
+            double lower = 0.5 * (time - discriminator);
+            double upper = 0.5 * (time + discriminator);
+            long lowerI = (long) Math.ceil(lower);
+            long upperI = (long) Math.floor(upper);
+            if (lowerI == lower) lowerI++;
+            if (upperI == upper) upperI--;
+            return upperI - lowerI + 1;
+        }
+    }
+}
