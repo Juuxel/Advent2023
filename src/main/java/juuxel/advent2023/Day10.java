2c87c659f72e726dc4450d8c4f12bc230ed0a620
Juuz <6596629+Juuxel@users.noreply.github.com>
2024-12-08 14:20:32 +0200

    Day 10, part 2

--- src/main/java/juuxel/advent2023/Day10.java
+++ src/main/java/juuxel/advent2023/Day10.java
@@ -2,30 +2,29 @@ package juuxel.advent2023;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.stream.Stream;
 
 public final class Day10 {
-    private static final int DOT = -1;
-    private static final int START = -2;
+    private static final int DOT = -2;
     private static final int VERT = -3;
     private static final int HORIZ = -4;
     private static final int F = -5;
     private static final int J = -6;
     private static final int L = -7;
     private static final int SEVEN = -8;
-    private static final int CCW = -1;
-    private static final int CW = 1;
-    private static final int RED = 1;
-    private static final int BLUE = 2;
 
     public static void main(String[] args) throws Exception {
-        part1(Loader.lines(10).toArray(String[]::new));
+        run(Loader.lines(10));
     }
 
-    public static void part1(String[] lines) throws Exception {
+    public static void run(Stream<String> lines) {
+        var grid = new CharGrid(lines);
+
+        // Part 1
         record Movement(int x, int y, int distance, Direction from) {}
 
-        int width = lines[0].length();
-        int height = lines.length;
+        int width = grid.width();
+        int height = grid.height();
 
         int max = 0;
         int[][] board = new int[width][height];
@@ -34,12 +33,14 @@ public final class Day10 {
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                board[x][y] = switch (lines[y].charAt(x)) {
-                    case 'S' -> {
-                        startX = x;
-                        startY = y;
-                        yield START;
-                    }
+                char c = grid.getChar(x, y);
+                if (c == 'S') {
+                    startX = x;
+                    startY = y;
+                    c = discoverS(grid, x, y);
+                }
+
+                board[x][y] = switch (c) {
                     case '-' -> HORIZ;
                     case '|' -> VERT;
                     case 'F' -> F;
@@ -59,7 +60,7 @@ public final class Day10 {
             if (x < 0 || y < 0 || x >= width || y >= height) continue;
             int dist = current.distance;
             int initial = board[x][y];
-            if (initial >= DOT) {
+            if (initial >= 0 || initial == DOT) {
                 continue;
             } else if (current.from != null) {
                 switch (current.from) {
@@ -81,86 +82,146 @@ public final class Day10 {
             board[x][y] = dist;
             max = Math.max(max, dist);
 
-            movements.add(new Movement(x - 1, y, dist + 1, Direction.RIGHT));
-            movements.add(new Movement(x + 1, y, dist + 1, Direction.LEFT));
-            movements.add(new Movement(x, y - 1, dist + 1, Direction.DOWN));
-            movements.add(new Movement(x, y + 1, dist + 1, Direction.UP));
+            if (initial == HORIZ || initial == SEVEN || initial == J) movements.add(new Movement(x - 1, y, dist + 1, Direction.RIGHT));
+            if (initial == HORIZ || initial == L || initial == F) movements.add(new Movement(x + 1, y, dist + 1, Direction.LEFT));
+            if (initial == VERT || initial == L || initial == J) movements.add(new Movement(x, y - 1, dist + 1, Direction.DOWN));
+            if (initial == VERT || initial == SEVEN || initial == F) movements.add(new Movement(x, y + 1, dist + 1, Direction.UP));
         } while ((current = movements.poll()) != null);
 
         System.out.println(max);
-    }
 
-    public static void part2(String[] lines) throws Exception {
-        enum MovementType {
-            PIPE, PAINT
-        }
-        record Movement(int x, int y, int value, MovementType type, Direction from) {}
+        // Part 2
+        var simulation = new Part2(expand(grid, board));
+        simulation.run();
 
-        int width = lines[0].length();
-        int height = lines.length;
-
-        int max = 0;
-        int[][] board = new int[width][height];
-        int[][] winding = new int[width][height];
-        int startX = -1;
-        int startY = -1;
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                board[x][y] = switch (lines[y].charAt(x)) {
-                    case 'S' -> {
-                        startX = x;
-                        startY = y;
-                        yield START;
-                    }
-                    case '-' -> HORIZ;
-                    case '|' -> VERT;
-                    case 'F' -> F;
-                    case 'J' -> J;
-                    case 'L' -> L;
-                    case '7' -> SEVEN;
-                    default -> DOT;
-                };
+        int part2 = 0;
+        for (int x = 0; x < grid.width(); x++) {
+            for (int y = 0; y < grid.height(); y++) {
+                if (board[x][y] < 0 && !simulation.visited.get(3 * x + 1, 3 * y + 1)) {
+                    part2++;
+                }
             }
         }
+        System.out.println(part2);
+    }
 
-        Queue<Movement> movements = new ArrayDeque<>();
-        Movement current = new Movement(startX, startY, 0, MovementType.PIPE, null);
-        int startWinding = 0;
-        do {
-            int x = current.x;
-            int y = current.y;
-            if (x < 0 || y < 0 || x >= width || y >= height) continue;
-            int initial = board[x][y];
-            if (initial >= 0) {
-                continue;
-            } else if (current.type == MovementType.PIPE && current.from != null) {
-                switch (current.from) {
-                    case UP -> {
-                        if (initial != VERT && initial != J && initial != L) continue;
+    private static char discoverS(CharGrid grid, int x, int y) {
+        boolean connectedLeft = grid.contains(x - 1, y) && "-FL".indexOf(grid.getChar(x - 1, y)) >= 0;
+        boolean connectedRight = grid.contains(x + 1, y) && "-7J".indexOf(grid.getChar(x + 1, y)) >= 0;
+        boolean connectedUp = grid.contains(x, y - 1) && "|7F".indexOf(grid.getChar(x, y - 1)) >= 0;
+        boolean connectedDown = grid.contains(x, y + 1) && "|JL".indexOf(grid.getChar(x, y + 1)) >= 0;
+
+        if (connectedUp && connectedDown) {
+            return '|';
+        } else if (connectedLeft && connectedRight) {
+            return '-';
+        } else if (connectedUp && connectedLeft) {
+            return 'J';
+        } else if (connectedDown && connectedLeft) {
+            return '7';
+        } else if (connectedUp && connectedRight) {
+            return 'L';
+        } else if (connectedDown && connectedRight) {
+            return 'F';
+        } else {
+            throw new RuntimeException("cannot figure out S");
+        }
+    }
+
+    private static BooleanGrid expand(CharGrid grid, int[][] part1Board) {
+        BooleanGrid obstacles = new BooleanGrid(3 * grid.width(), 3 * grid.height());
+        for (int x = 0; x < grid.width(); x++) {
+            for (int y = 0; y < grid.height(); y++) {
+                int ox = 3 * x;
+                int oy = 3 * y;
+
+                char c = grid.getChar(x, y);
+                if (c == 'S') {
+                    c = discoverS(grid, x, y);
+                } else if (part1Board[x][y] < 0) {
+                    continue;
+                }
+
+                switch (c) {
+                    case '|' -> {
+                        obstacles.mark(ox + 1, oy);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox + 1, oy + 2);
                     }
-                    case DOWN -> {
-                        if (initial != VERT && initial != SEVEN && initial != F) continue;
+                    case '-' -> {
+                        obstacles.mark(ox, oy + 1);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox + 2, oy + 1);
                     }
-                    case LEFT -> {
-                        if (initial != HORIZ && initial != J && initial != SEVEN) continue;
+                    case 'L' -> {
+                        obstacles.mark(ox + 1, oy);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox + 2, oy + 1);
                     }
-                    case RIGHT -> {
-                        if (initial != HORIZ && initial != L && initial != F) continue;
+                    case 'J' -> {
+                        obstacles.mark(ox + 1, oy);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox, oy + 1);
+                    }
+                    case '7' -> {
+                        obstacles.mark(ox + 1, oy + 2);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox, oy + 1);
+                    }
+                    case 'F' -> {
+                        obstacles.mark(ox + 1, oy + 2);
+                        obstacles.mark(ox + 1, oy + 1);
+                        obstacles.mark(ox + 2, oy + 1);
                     }
                 }
             }
+        }
+        return obstacles;
+    }
 
-            board[x][y] = dist;
-            max = Math.max(max, dist);
+    private static final class Part2 {
+        private final BooleanGrid obstacles;
+        private final BooleanGrid visited;
+        private final Queue<Point> destinations = new ArrayDeque<>();
 
-            movements.add(new Movement(x - 1, y, dist + 1, Direction.RIGHT));
-            movements.add(new Movement(x + 1, y, dist + 1, Direction.LEFT));
-            movements.add(new Movement(x, y - 1, dist + 1, Direction.DOWN));
-            movements.add(new Movement(x, y + 1, dist + 1, Direction.UP));
-        } while ((current = movements.poll()) != null);
+        private Part2(BooleanGrid obstacles) {
+            this.obstacles = obstacles;
+            this.visited = new BooleanGrid(obstacles.width(), obstacles.height());
+        }
 
-        System.out.println(max);
+        public void run() {
+            // Add outsides
+            for (int x = 0; x < obstacles.width(); x++) {
+                addNeighbours(x, -1);
+                addNeighbours(x, obstacles.height());
+            }
+
+            for (int y = 0; y < obstacles.height(); y++) {
+                addNeighbours(-1, y);
+                addNeighbours(obstacles.width(), y);
+            }
+
+            Point destination;
+            while ((destination = destinations.poll()) != null) {
+                // Skip out-of-bounds destinations
+                if (!obstacles.contains(destination.x, destination.y)) continue;
+                if (obstacles.get(destination.x, destination.y)) continue;
+
+                if (visited.mark(destination.x, destination.y)) {
+                    addNeighbours(destination.x, destination.y);
+                }
+            }
+        }
+
+        private void addNeighbours(int x, int y) {
+            destinations.add(new Point(x - 1, y));
+            destinations.add(new Point(x + 1, y));
+            destinations.add(new Point(x, y - 1));
+            destinations.add(new Point(x, y + 1));
+        }
+    }
+
+    private record Point(int x, int y) {
     }
 
     private enum Direction {
---
+++ src/main/java/juuxel/advent2023/Grid.java
@@ -0,0 +1,39 @@
+package juuxel.advent2023;
+
+import java.util.function.Function;
+
+public interface Grid<T> {
+    int width();
+    int height();
+    T get(int x, int y);
+
+    default <U> Grid<U> map(Function<T, U> transform) {
+        ArrayGrid<U> mapped = new ArrayGrid<>(width(), height());
+
+        for (int x = 0; x < mapped.width(); x++) {
+            for (int y = 0; y < mapped.height(); y++) {
+                mapped.set(x, y, transform.apply(get(x, y)));
+            }
+        }
+
+        return mapped;
+    }
+
+    default boolean contains(int x, int y) {
+        return 0 <= x && x < width() && 0 <= y && y < height();
+    }
+
+    default String prettyPrint(Function<T, String> displayer) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int y = 0; y < height(); y++) {
+            if (y > 0) sb.append('\n');
+
+            for (int x = 0; x < width(); x++) {
+                sb.append(displayer.apply(get(x, y)));
+            }
+        }
+
+        return sb.toString();
+    }
+}
